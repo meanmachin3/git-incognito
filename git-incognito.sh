1ff8ceb9ef5b7fd04769101dccb4bf0e37e80152
Manish Yadav <yadavmanish1607@gmail.com>
2019-06-06 22:01:23 +0530

    feat: Turn off incognito mode

--- git-incognito.sh
+++ git-incognito.sh
@@ -1,2 +1,15 @@
-git config --local user.name Bob
-git config --local user.email bob@example.com
+#!/bin/sh
+
+if [[ $# -eq 0 ]] ; then
+  git config --local user.name Bob
+  git config --local user.email bob@example.com
+  echo "You have now gone incognito"
+elif [ $1 = "off" ] ; then
+  username=$(git config --global user.name)
+  email=$(git config --global user.email)
+  git config --local user.name "$username"
+  git config --local user.email "$email"
+  echo "Disabled incognito mode"
+else
+  echo "Invalid arguments supplied"
+fi
